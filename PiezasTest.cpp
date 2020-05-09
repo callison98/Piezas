9abde1dc96ff851b018bccd3cbf9222939196414
callison98 <callison4@mail.csuchico.edu>
2020-05-08 22:03:31 -0700

    Added few tests

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -22,18 +22,18 @@ TEST(PiezasTest, sanityCheck)
 TEST(PiezasTest, Check_First_Value_After_Constructor)
 {
 	Piezas ptest;
-	ASSERT_EQ(Blank, ptest.pieceAt(0,0))
+	ASSERT_EQ(Blank, ptest.pieceAt(0,0));
 }
 TEST(PiezasTest, Check_Drop)
 {
 	Piezas ptest;
-	ptest.dropPiece(3)
-	ASSERT_EQ(X, ptest.pieceAt(0,3))
+	ptest.dropPiece(3);
+	ASSERT_EQ(X, ptest.pieceAt(0,3));
 }
 TEST(PiezasTest, Check_Reset)
 {
 	Piezas ptest;
-	ptest.dropPiece(3)
-	ptest.reset()
-	ASSERT_EQ(Blank, ptest.pieceAt(0,3))
+	ptest.dropPiece(3);
+	ptest.reset();
+	ASSERT_EQ(Blank, ptest.pieceAt(0,3));
 }
